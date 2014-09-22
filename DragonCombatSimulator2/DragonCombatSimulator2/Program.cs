6550e946115bb3c12c376964cff1856eb619fc07
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-09-22 12:26:30 -0600

    ok

--- DragonCombatSimulator2/DragonCombatSimulator2/Program.cs
+++ DragonCombatSimulator2/DragonCombatSimulator2/Program.cs
@@ -246,7 +246,52 @@ namespace DragonCombatSimulator2
                
                 //print to console how many points dragon has to this point
                 Console.WriteLine("And Dragon has " + dragonHitPoints + " point");
-                Console.WriteLine(" ");
+                Console.WriteLine(" ");{
+                }
+
+    
+
+            
+             static void AddHighScore(int PlayerScore)
+        {
+            Console.WriteLine("Your Name");
+            string playerName = Console.ReadLine();
+            NORTHWNDEntities db = new NORTHWNDEntities();
+
+            //create a new highscore
+            HighScore newHighScore = new HighScore();
+            newHighScore.Date = DateTime.Now;
+            newHighScore.Game = "Dragon Combat";
+            newHighScore.Name = playerName;
+            newHighScore.Score = PlayerScore;
+
+            //add it to the data base
+
+            db.HighScores.Add(newHighScore);
+
+            //save changes 
+            db.SaveChanges();
+            
+
+        }
+
+        static void DisplayHighScore()
+        {
+            Console.WriteLine("Display Dragon Combat High Score");
+            Console.WriteLine("--------------");
+
+            NORTHWNDEntities db = new NORTHWNDEntities();
+            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Dragon Combat")
+                .OrderByDescending(x => x.Score).Take(10).ToList();
+
+            foreach(HighScore highScore in highScoreList)
+            {
+                Console.WriteLine("{0}. {1}- {2} on {3}",
+                highScoreList.IndexOf(highScore) + 1,
+                highScore.Name,
+                highScore.Score,
+                highScore.Date.Value.ToShortDateString());
+  
             }
         }
 
